387ba639137756b4c95b26c3451fd3d5cf9a5ab4
Caleb16 <crank1@swceschool.org>
2015-02-04 17:15:02 -0700

    Added Files

---
+++ Odd-Number-Sum-Assignment/Odd-Number-Sum-Assignment/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.txtOddNum = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(40, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(249, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter An Number Greater Than 1"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(107, 102)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(89, 39)
+        Me.btnCalculate.TabIndex = 1
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResult.Location = New System.Drawing.Point(7, 162)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(0, 15)
+        Me.lblResult.TabIndex = 2
+        '
+        'txtOddNum
+        '
+        Me.txtOddNum.Location = New System.Drawing.Point(82, 64)
+        Me.txtOddNum.Name = "txtOddNum"
+        Me.txtOddNum.Size = New System.Drawing.Size(141, 20)
+        Me.txtOddNum.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(326, 201)
+        Me.Controls.Add(Me.txtOddNum)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Odd Number Sum"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+    Friend WithEvents txtOddNum As System.Windows.Forms.TextBox
+
+End Class
---
+++ Odd-Number-Sum-Assignment/Odd-Number-Sum-Assignment/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim intOddNum As Integer = Val(Me.txtOddNum.Text)
+        Dim intFactor As Integer = 1
+        Dim intFactorCounter As Integer = 0
+
+        Dim intCounter As Integer
+        For intCounter = 1 To intOddNum
+            If intFactor Mod 2 = 1 Then
+                intFactor = intFactor + intCounter
+            ElseIf intFactor Mod 2 = 0 Then
+                intFactor = intFactor + 1
+                intFactorCounter += 1
+            End If
+        Next intCounter
+
+        If intOddNum = 1 Then
+            Me.lblResult.Text = "Follow the Rules. Pick Another Number Greater than 1."
+        Else
+            Me.lblResult.Text = "The Sum of all Odd Numbers Between 1 and " & intOddNum & " is " & (intFactor - intFactorCounter - 1)
+            'The intFactorCounter is because my answers were coming out too high because of the intFactor = intFactor + 1. 
+            'I had to do this because otherwise it would just get stuck on this part of the code and it wouldn't work continue to count up.
+
+            '-1 is because after I added the intFactor counter, for some reason all of my answers kept coming out one too high.
+            'I tested it with these changes with several random numbers and used my calculator to check the answers. They all came out correct.
+            'I'm sure this isn't very professional (I hope you don't mind) but at least it works perfectly.
+        End If
+    End Sub
+End Class
